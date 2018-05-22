efd1cdeada744730850a6375147d0c282948a697
Ninad Sakhardande <ninad.sakhardande@gmail.com>
2018-05-22 21:00:45 +0530

    Improved the suggestions.

--- dict.py
+++ dict.py
@@ -10,7 +10,16 @@ def find_definition(word) :
 	if word in data:
 		return data[word]
 	elif len(get_close_matches(word, data.keys())) > 0 :
-		return "The word doesn't exist in the dictionary. Did you mean one of these words?: {}".format(get_close_matches(word,data.keys()))
+		yon = input( "The word doesn't exist in the dictionary. Did you mean one of these words?: {}. Enter Y for Yes and N for No: ".format(get_close_matches(word,data.keys())))
+
+		if yon == 'Y' :
+			new_word = input("Enter the word from the given choices: ")
+			new_word = new_word.lower()
+			return data[new_word]
+		elif yon == 'N' :
+			return "No entry found. Please enter another word."
+		else :
+			return "Invalid Entry."
 	else:
 		return "No entry found. Please enter another word."
 
